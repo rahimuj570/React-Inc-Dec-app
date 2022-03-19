b98bd733580cfad5f7e7ad606cf0699fff63e336
rahimuj570 <rahimuj570@gmail.com>
2022-03-20 00:49:41 +0600

    decrease button is working

--- src/Components/Decrease/Decrease.js
+++ src/Components/Decrease/Decrease.js
@@ -1,9 +1,17 @@
 import React from "react";
 
-const Decrease = () => {
+const Decrease = (props) => {
+  const [value, setValue] = props.propValue;
+  const decBtn = () => {
+    if (value > 0) {
+      setValue(value - 1);
+    } else {
+      alert(`Can't Decrease Negative`);
+    }
+  };
   return (
     <div>
-      <button>Decrease</button>
+      <button onClick={decBtn}>Decrease</button>
     </div>
   );
 };
